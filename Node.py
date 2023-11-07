e91cffdf25d81b0dd689199db883d03362929bdf
ezequicruz <ezequicruz14@outlook.com>
2023-11-06 22:55:48 -0600

    Insertion of the array of adjacency, with the structure [[#Successors,[Successor,Weight]]]

--- Node.py
+++ Node.py
@@ -6,4 +6,7 @@ class Node():
 
     def __str__(self):
         return  f'Node: {self.num_node} | #Successors: {self.num_successors} | Successors: {self.successors}'
-
+print("TEST NODE".center(50,'-'))
+Node1 = Node(1,3,[6,13,4])
+print(Node1)
+print(len(Node1.successors))
---
+++ main.py
@@ -0,0 +1,5 @@
+#IMPORT CLASS NODE
+from Node import Node
+
+node2 = Node(2,3,[22,12,9])
+print(node2)
